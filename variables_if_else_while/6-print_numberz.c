bb6143ad4ef65c81831f9cc92c3b6abd1b85ad61
Litoxam <litobernier@gmail.com>
2026-02-24 15:05:21 +0100

    Numberz with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+#include <unistd.h>
+
+int main(void)
+{
+char c = '0';
+
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+printf("\n");
+return (0);
+}
