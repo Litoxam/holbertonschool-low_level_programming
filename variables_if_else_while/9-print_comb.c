59285a6e8ffdde589494f62ff76021f28822ce7e
Litoxam <litobernier@gmail.com>
2026-02-24 15:47:09 +0100

    Print combinaisons

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+#include <unistd.h>
+
+int main(void)
+{
+
+char c = '0';
+
+while (c <= '9')
+{
+putchar(c);
+putchar(',');
+putchar(' ');
+c++;
+}
+
+putchar('\n');
+return (0);
+}
