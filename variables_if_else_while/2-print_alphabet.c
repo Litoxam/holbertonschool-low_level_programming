16eb96ab96e1d92d9d624b22b717596fa445c9b2
Litoxam <litobernier@gmail.com>
2026-02-24 13:27:31 +0100

    Print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+putchar ('a');
+putchar ('b');
+putchar ('c');
+putchar ('d');
+putchar ('e');
+putchar ('f');
+putchar ('g');
+putchar ('h');
+putchar ('i');
+putchar ('j');
+putchar ('k');
+putchar ('l');
+putchar ('m');
+putchar ('n');
+putchar ('o');
+putchar ('p');
+putchar ('q');
+putchar ('r');
+putchar ('s');
+putchar ('t');
+putchar ('u');
+putchar ('v');
+putchar ('w');
+putchar ('x');
+putchar ('y');
+putchar ('z');
+putchar ('\n');
+	return (0);
+}
