6cf7c7af00bf1fc7ea41b8d44520538a076e5512
Litoxam <litobernier@gmail.com>
2026-02-24 15:01:55 +0100

    Print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+#include <unistd.h>
+
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("\n");
+return (0);
+}
