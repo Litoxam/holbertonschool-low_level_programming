050fa080f64d71da42c1346af7d38c0c9c00ad3f
Litoxam <litobernier@gmail.com>
2026-02-25 21:33:39 +0100

    Print until 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry
+ * @n: base number, change it until 98
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+
+	while (n <= 98)
+	{
+	printf("%d, ", n);
+	n++;
+	}
+	putchar('\n');
+}
