9a832396d5894e007be437f72dd467af0cd4d7fa
Litoxam <litobernier@gmail.com>
2026-02-25 14:42:01 +0100

    Print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - check if c is a letter
+ *
+ * @n: The characters to check;
+ *
+ * Return: last digit of n, if negative, returns -n
+ */
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = n % 10;
+
+	if (lastDigit < 0)
+		lastDigit = -lastDigit;
+
+	_putchar(lastDigit + '0');
+
+	return (lastDigit);
+}
