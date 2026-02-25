98cf70cdbde178170380db293e4b13d6d92d1076
Litoxam <litobernier@gmail.com>
2026-02-25 14:05:56 +0100

    Check if positive or negative

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,19 +11,21 @@
 int print_sign(int n)
 
 {
-	if (n >= 1)
+	if (n > 1)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n <= 0)
+	if (n < 0)
 	{
-		return (0);
 		_putchar('-');
-	}
-	else
-	{
 		return (0);
-		_putchar('0');
 	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+		
+	}
+	return (0);
 }
