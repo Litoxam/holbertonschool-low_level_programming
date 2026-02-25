20ae65a0afdf045004fcc3f5092acbe6ca4609d5
Litoxam <litobernier@gmail.com>
2026-02-25 13:58:04 +0100

    Check if positive or negative

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _isalpha - check if c is a letter
+ * print_sign - check if c is a letter
  *
  * @n: The characters to check;
  *
- * Return: if c is a letter, returns 1; else returns 0
+ * Return: if n is positive, return 1 and +, else return 0 and -
+ * if =0 return 0 and 0
  */
-int _isalpha(int n)
+int print_sign(int n)
 
 {
 	if (n >= 1)
