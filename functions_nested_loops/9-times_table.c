12d1463b96fa4879242a1584605125468cd08310
Litoxam <litobernier@gmail.com>
2026-02-25 20:37:27 +0100

    Times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Entry
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int line;
+	int c;
+	int result;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			result = line * c;
+
+			if (result >= 10)
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+
+			if (c != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
