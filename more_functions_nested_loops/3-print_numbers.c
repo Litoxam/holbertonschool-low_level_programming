52fa350eec9e1677fd887fe97ce1bb93fd445ecd
Litoxam <litobernier@gmail.com>
2026-02-26 15:52:15 +0100

    Print all the numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - prints the numbers
+ *
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	_putchar(n);
+}
+_putchar('\n');
+}
