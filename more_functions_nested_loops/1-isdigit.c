33f78600ab6b0d68027e13f6082c6c73bb95cc4e
Litoxam <litobernier@gmail.com>
2026-02-26 15:33:06 +0100

    Check if digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _isdigit - int a character.
+ * @c: character
+ *
+ * Return: 1 if digit, 0 if anything else
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+}
