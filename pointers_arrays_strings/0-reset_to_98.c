2590a58ebc049668a67924128542438b1d036356
Litoxam <litobernier@gmail.com>
2026-03-03 09:46:37 +0100

    Adding the directory and the task 0, with the main.h

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - change the value of n with pointer
+ * @n: initial value
+ * Return: no return.
+ */
+
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
