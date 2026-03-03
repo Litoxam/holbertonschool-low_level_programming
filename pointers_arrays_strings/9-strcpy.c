a96414248bb29e44c97be07233b487fa3fa5ec0d
Litoxam <litobernier@gmail.com>
2026-03-03 22:55:31 +0100

    Copy a string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy a string
+ * @dest: destination where the source is copied
+ * @src: string source
+ *
+ * Return: value of dest .
+ */
+
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
