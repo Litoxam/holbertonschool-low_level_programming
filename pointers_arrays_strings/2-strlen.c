8eb323bb03350d78486f5d6c4cbad48a0ea2469a
Litoxam <litobernier@gmail.com>
2026-03-03 13:09:44 +0100

    Length of a string 16

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: points toward str in this exercice
+ *
+ * Return: length of the string.
+ */
+
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+	length++;
+	s++;
+	}
+	return (length);
+
+}
