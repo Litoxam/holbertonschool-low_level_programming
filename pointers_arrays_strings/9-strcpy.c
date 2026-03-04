cc47053c6e41e2e0bd8206525857a517d716d6a4
Litoxam <litobernier@gmail.com>
2026-03-04 09:15:38 +0100

    Forgot about the \0 at the end of the string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcpy - copy a string
@@ -17,6 +16,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[i] = src[i];
-
+	
+	dest[i] = '\0';
 	return (dest);
 }
