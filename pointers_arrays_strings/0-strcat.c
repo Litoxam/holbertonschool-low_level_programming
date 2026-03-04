3de754f68e87060baa908ec8a60a3ce3f42a2e91
Litoxam <litobernier@gmail.com>
2026-03-04 18:23:26 +0100

    Correcting the code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,20 +13,20 @@ char *_strcat(char *dest, char *src)
 	int here = 0; /* where i am in the string*/
 	int second = 0; /* copying the second string*/
 
-	while (dest[here] != '\0')
+	while (dest[here] != '\0') /*we move at the end of first string*/
 	{
 		here++;
 	}
 
-	while (src[second] != '\0')
+	while (src[second] != '\0') /*we add 2d string after 1st*/
 	{
-		dest[here] = src[here];
+		dest[here] = src[second];
 		here++;
 		second++;
 	}
 
 
-	dest[here] = '\0';
+	dest[here] = '\0'; /* we add a \0 at the end*/
 
-	return (dest);
+	return (dest); /* returning first + second string*/
 }
