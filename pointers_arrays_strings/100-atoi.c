b3abed182aaf401ff853dd1a3caa96ab6a0b8ab3
Litoxam <litobernier@gmail.com>
2026-03-04 10:12:45 +0100

    Atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ *
+ * Return: integer value
+ */
+
+int _atoi(char *s)
+{
+	int here = 0;
+	int sign = 1;
+	int num = 0;
+
+	while (s[here] != '\0')
+	{
+		if (s[here] == '-')
+			sign = sign * (-1); /*change the sign everytime there is a '-'*/
+
+		if (s[here] >= '0' && s[here] <= '9') /*detect the first digit then stop*/
+		{
+			break;
+		}
+
+		here++;
+	}
+
+	while (s[here] >= '0' && s[here] <= '9') /*while 'here' is between 0 and 9*/
+	{
+		num = num * 10 + (s[here] - '0'); /* convert the value from char to int */
+		here++;
+	}
+
+	return (num * sign); /*final sign of the result*/
+}
