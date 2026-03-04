088d9d322b4e59b4a75eaffdac59eaa1d3eec1ca
Litoxam <litobernier@gmail.com>
2026-03-04 10:38:55 +0100

    Adding the digit variable to detect if there is a digit in the string

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,6 +12,7 @@ int _atoi(char *s)
 	int here = 0;
 	int sign = 1;
 	int num = 0;
+	int digit = 0;
 
 	/*detect the first digit*/
 	while (s[here] != '\0' && (s[here] < '0' || s[here] > '9'))
@@ -20,13 +21,19 @@ int _atoi(char *s)
 			sign = sign * (-1); /*change the sign everytime there is a '-'*/
 
 		here++;
+
 	}
 
 	while (s[here] >= '0' && s[here] <= '9') /*while 'here' is between 0 and 9*/
 	{
+		digit = 1;
 		num = num * 10 + (s[here] - '0'); /* convert the value from char to int */
 		here++;
 	}
+
+	if (digit == 0)
+	return (0);
+
 
 	return (num * sign); /*final sign of the result*/
 }
