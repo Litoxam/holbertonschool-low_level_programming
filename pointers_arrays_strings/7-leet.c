7cde7f91e2443b91ce89072716623cb6e27ff04a
Litoxam <litobernier@gmail.com>
2026-03-05 21:41:10 +0100

    Leet programm

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,14 +11,14 @@ char *leet(char *str)
 {
 	int i;
 	int scan;
-	char leet[] = "aeotl";
-	char change[] = "43071";
+	char leet[] = "aeotlAEOTL";
+	char change[] = "4307143071";
 
 for (i = 0; str[i] != '\0'; i++) /*check the string*/
 {
 	for (scan = 0; leet[scan] != '\0'; scan++) 
 	{
-	if (str[i] == leet[scan] || str[i] == leet[scan] - 32) /*if it's in the list*/
+	if (str[i] == leet[scan]) /*if it's in the list*/
 		{
 		str[i] = change[scan]; /*into a number*/
 		}
