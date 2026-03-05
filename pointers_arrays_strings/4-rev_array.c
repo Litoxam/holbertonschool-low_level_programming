d18d4bb9485824c9872780b7ee76363ca730f716
Litoxam <litobernier@gmail.com>
2026-03-05 14:11:50 +0100

    Print a reversed array of integers

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *reverse_array - reverse the values of an array
+ * @a : array of integers
+ * @n : the number of elements to swap
+ *
+ * Return: no return
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	for (i = 0 ; i < (n / 2) ; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
