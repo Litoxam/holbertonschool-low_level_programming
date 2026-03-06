75480eb4c9bdac294376e97b9db3012e677b9281
Litoxam <litobernier@gmail.com>
2026-03-06 16:22:34 +0100

    Adding the directory and task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - displays the program name
+ * @argc: number of arguments
+ * @argv: array of the arguments
+ * Return: return 0;
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
