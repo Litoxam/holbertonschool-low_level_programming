e8a9730f4730c0bc5af563982fc9e29ebcb2e330
Litoxam <litobernier@gmail.com>
2026-03-06 16:30:00 +0100

    changed the description

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - displays the program name
+ * main - displays the number of arguments
  * @argc: number of arguments
  * @argv: array of the arguments
  * Return: return 0;
