e2b8ba680e570c2cee56cce3efa6a719deab46c3
Litoxam <litobernier@gmail.com>
2026-03-06 16:29:36 +0100

    Display the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - displays the program name
+ * @argc: number of arguments
+ * @argv: array of the arguments
+ * Return: return 0;
+ */
+
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
