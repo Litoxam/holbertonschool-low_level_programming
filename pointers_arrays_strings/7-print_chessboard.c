ef1e4765d4a0133caa534e31d0a2524bc822b2c9
Litoxam <litobernier@gmail.com>
2026-03-06 19:34:20 +0100

    Chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - print a chessboard
+ * @a: initial array
+ *
+ * Return: no return
+ */
+void print_chessboard(char (*a)[8])
+{
+
+	int i;
+	int col;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		for (col = 0; i < 8; col++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar('\n');
+	}
+
+}
