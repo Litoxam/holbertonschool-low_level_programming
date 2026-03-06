a271b7c7354ab2a5f28b43a8347221b0a63432bc
Litoxam <litobernier@gmail.com>
2026-03-06 18:15:41 +0100

    added a loop inside the first one to check every char of the string accept

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int i, j;
 	unsigned int count = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == *accept)
+		for (j = 0; accept[j]; j++)
 		{
-		count += 1;
+			if (s[i] == accept[j])
+			{
+				count += 1;
+			}
 		}
-		i++;
+
 	}
-
-
 	return (count);
 }
