faf08ea912d5a7f56504bf48dd002094a865ea4a
Litoxam <litobernier@gmail.com>
2026-03-06 11:13:06 +0100

    Looking for a char in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strchr - look for a char
+ * @s: initial string
+ * @c: char we are looking for
+ *
+ *
+ * Return: the encoded string
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+		return (&s[i]);
+		}
+
+		if (s[i] == '\0')
+		{
+		return (&s[i]);
+		}
+
+	}
+
+		return (0);
+}
