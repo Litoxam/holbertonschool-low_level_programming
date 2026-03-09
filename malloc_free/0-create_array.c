051b0b66b93002f436272f049de189838f32b900
Litoxam <litobernier@gmail.com>
2026-03-09 14:00:48 +0100

    Adding the directory and task 0 + readme

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+ * *create_array - copy a string
+ * @size: size of the array
+ * @c: char initialized
+ *
+ * Return: NULL it size à or if malloc fails; return str .
+ */
+
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
+
+
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+return (str);
+
+}
