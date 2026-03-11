ce313040afc3e1892eb18918d162ebbf712ecc66
Litoxam <litobernier@gmail.com>
2026-03-11 16:20:20 +0100

    Initializes a variable of type struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: no return
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	/**
+	 * accède à l'adresse stockée dans d, trouve la structure,
+	 *
+	 * et accède au champ name, age et owner.
+	 */
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
