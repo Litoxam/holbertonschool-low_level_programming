1e89667eeedaea46066e4cdfda28d1cebb3c8875
Litoxam <litobernier@gmail.com>
2026-03-16 17:14:04 +0100

    Length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - renvoie la longueur d'une chaîne.
+ * @s: la chaîne à mesurer.
+ *
+ * Return: la longueur de la chaîne.
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
