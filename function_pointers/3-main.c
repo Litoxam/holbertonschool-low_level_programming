b77e056d4a76e812cd12c3d067683e8700cebfd1
Litoxam <litobernier@gmail.com>
2026-03-20 11:54:11 +0100

    changed the order of some commands

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,18 +12,16 @@
 
 int main(int argc, char *argv[])
 {
+	int a, b;
 	int (*f)(int, int);
-
-	f = get_op_func(argv[2]);
-
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
-
+	
 	if (argc != 4)
 	{
 	printf("Error\n");
 	exit(98);
 	}
+
+	f = get_op_func(argv[2]);
 
 
 	/*si l'opérateur n'est pas dans la liste*/
@@ -32,6 +30,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
 
 	/*si on divise ou module par 0, erreur 100*/
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
