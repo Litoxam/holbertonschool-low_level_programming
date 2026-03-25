d25a310c5302308ef3e24faff0504713080a95d3
Litoxam <litobernier@gmail.com>
2026-03-25 17:22:49 +0100

    returns the number of elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * list_len - return the number of elements in a linked list
+ * @h: points to the head
+ *
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
