7e8613cb2ed53579e1fcd1242df8ab1becaa963b
Litoxam <litobernier@gmail.com>
2026-04-05 16:29:38 +0200

    Add directory, task 0, README.md and the hash_tables.h

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array (number of buckets).
+ *
+ * Return: A pointer to the newly created hash table,
+ * or NULL if something went wrong (ex : malloc failed)
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = calloc(size, sizeof(hash_node_t *));
+
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	new_table->size = size;
+
+	return (new_table);
+}
