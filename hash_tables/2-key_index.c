814ccd8185b92b586bed081736f8c1f8e6dc9bd3
Litoxam <litobernier@gmail.com>
2026-04-05 16:59:57 +0200

    Hashing a string with djb2 and return hash % size

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+
+/**
+ * key_index - Gives the index of a key.
+ * @key: The key (string) to hash.
+ * @size: The size of the hash table array.
+ *
+ * Return: The index for the key/value pair.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}
