48253b7354339e53e5d3aeda11e6f360f10772c3
Litoxam <litobernier@gmail.com>
2026-04-12 00:48:45 +0200

    Betty style, and some error conditions added on the read_textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,20 +23,30 @@ fd = open(filename, O_RDONLY); /*we open the file to scan it*/
 if (fd == -1)
 	return (0);
 
-buffer = malloc(sizeof(char)*(letters)); /* we allocate a buffer*/
+buffer = malloc(sizeof(char) * (letters)); /* we allocate a buffer*/
 
 if (!buffer)
+{
+	close(fd);
 	return (0);
-
+}
 /*we scan filename which is in fd, we stock "letters"*chars on the buffer*/
 nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 /*write the text contained in the buffer, number of char returned by nread*/
 nwrote = write(STDOUT_FILENO, buffer, nread);
 
 close(fd);
-
 free(buffer);
 
-return(nwrote);
+	if (n_wrote == -1 || n_wrote != n_read)
+		return (0);
+
+return (nwrote);
 
 }
--- file_io/main.h
+++ file_io/main.h
@@ -23,4 +23,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 
 
 
-#endif 
+#endif
