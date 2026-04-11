70dcbac9132177e3c98be14d72c1e8f26ef1fefe
Litoxam <litobernier@gmail.com>
2026-04-12 01:04:44 +0200

    Create a file and write tex_content in it

--- file_io/main.h
+++ file_io/main.h
@@ -6,16 +6,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /*open*/
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 
 /*Prototypes*/
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 
 
